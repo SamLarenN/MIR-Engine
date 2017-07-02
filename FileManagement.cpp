6932b94030c5cf4c3fd78caa096483857091e777
SamLarenN <ephixn@gmail.com>
2017-07-02 20:51:48 +0200

    Update FileManagement.cpp

--- FileManagement.cpp
+++ FileManagement.cpp
@@ -1,66 +1,92 @@
 #include "include.h"
-FileManagement* FManager = new FileManagement();
 
 
-void FileManagement::OnSetup(const char* FileName)
+
+FileManagement::FileManagement(const std::string FileName) : m_FileName(FileName)
 {
-	if (!ReadExistingFile(FileName))
+	if (!ReadExistingFile())
 	{
-		printf("%d \n", GetLastError());
-		CloseHandle(hFile);
+		printf("Error: 0x%X \n", GetLastError());
 		system("pause");
 		return;
 	}
-}
 
-FileManagement::FileManagement()
-{
+	if (!GetTextSection())
+	{
+		printf("Getting .text section failed\n");
+		system("pause");
+		return;
+	}
 }
 
 
 FileManagement::~FileManagement()
 {
-	if (!VirtualFree(pBytes, dFileSize, MEM_DECOMMIT))
+	if (!VirtualFree(m_pBytes, m_dwFileSize, MEM_DECOMMIT))
 	{
 		printf("%s \n", "Memory Deallocation Could Not Be Performed");
 		system("pause");
 		return;
 	}
-	CloseHandle(hFile);
-	pBytes = nullptr;
-	hFile = nullptr;
+	CloseHandle(m_hFile);
+	m_pBytes = nullptr;
+	m_hFile = nullptr;
 }
 
 
-bool FileManagement::ReadExistingFile(const char* FileName)
+bool FileManagement::GetTextSection()
 {
-	hFile = CreateFile(FileName, GENERIC_READ, NULL, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (hFile == INVALID_HANDLE_VALUE)
+	PIMAGE_DOS_HEADER pDosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(m_pBytes);
+	if (pDosHeader->e_magic != IMAGE_DOS_SIGNATURE)
+		return 0;
+	PIMAGE_NT_HEADERS pNtHeader = reinterpret_cast<PIMAGE_NT_HEADERS>((DWORD)pDosHeader + pDosHeader->e_lfanew);
+	if (pNtHeader->Signature != IMAGE_NT_SIGNATURE)
+		return 0;
+
+	PIMAGE_OPTIONAL_HEADER pOpt = reinterpret_cast<PIMAGE_OPTIONAL_HEADER>(&pNtHeader->OptionalHeader);
+	PIMAGE_SECTION_HEADER sec = reinterpret_cast<PIMAGE_SECTION_HEADER>((DWORD)pOpt + pNtHeader->FileHeader.SizeOfOptionalHeader);
+
+	for (int i = 0; i < pNtHeader->FileHeader.NumberOfSections; ++i)
+	{
+		PIMAGE_SECTION_HEADER pSection = sec + i;
+		if (!strcmp(reinterpret_cast<const char*>(pSection->Name), ".text"))
+		{
+			pTextSection = pSection;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+bool FileManagement::ReadExistingFile()
+{
+	m_hFile = CreateFile(m_FileName.c_str(), GENERIC_READ, NULL, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (m_hFile == INVALID_HANDLE_VALUE)
 	{
 		printf("%s \n", "No Access To File");
 		return 0;
 	}
 
-	dFileSize = GetFileSize(hFile, NULL);
-	if (dFileSize == INVALID_FILE_SIZE)
+	m_dwFileSize = GetFileSize(m_hFile, NULL);
+	if (m_dwFileSize == INVALID_FILE_SIZE)
 	{
 		printf("%s \n", "Could Not Get File Size");
-		CloseHandle(hFile);
+		CloseHandle(m_hFile);
 		return 0;
 	}
 
-	pBytes = VirtualAlloc(NULL, dFileSize, MEM_COMMIT, PAGE_READWRITE);
-	if (pBytes == NULL)
+	m_pBytes = VirtualAlloc(NULL, m_dwFileSize, MEM_COMMIT, PAGE_READWRITE);
+	if (m_pBytes == NULL)
 	{
 		printf("%s \n", "No Memory Could Be Allocated For The Code");
-		CloseHandle(hFile);
+		CloseHandle(m_hFile);
 		return 0;
 	}
 
-	if (!ReadFile(hFile, pBytes, dFileSize, NULL, NULL))
+	if (!ReadFile(m_hFile, m_pBytes, m_dwFileSize, NULL, NULL))
 	{
 		printf("%s \n", "Could Not Read Bytes");
-		CloseHandle(hFile);
+		CloseHandle(m_hFile);
 		return 0;
 	}
 
@@ -68,39 +94,39 @@ bool FileManagement::ReadExistingFile(const char* FileName)
 }
 
 
-bool FileManagement::WriteNewFile(const char* NewFileName, void* Bytes, DWORD NumberOfBytesToWrite)
+bool FileManagement::WriteNewFile(const std::string NewFileName, void* Bytes, const DWORD NumberOfBytesToWrite)
 {
-	hNewFile = CreateFile(NewFileName, GENERIC_WRITE, NULL, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (hNewFile == INVALID_HANDLE_VALUE)
+	m_hNewFile = CreateFile(NewFileName.c_str(), GENERIC_WRITE, NULL, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (m_hNewFile == INVALID_HANDLE_VALUE)
 	{
 		printf("%s \n", "Could Not Create New File");
 		printf("%d \n", GetLastError());
 		return 0;
 	}
 
-	if (!WriteFile(hNewFile, Bytes, NumberOfBytesToWrite, NULL, NULL))
+	if (!WriteFile(m_hNewFile, Bytes, NumberOfBytesToWrite, NULL, NULL))
 	{
-		CloseHandle(hNewFile);
+		CloseHandle(m_hNewFile);
 		printf("%s \n", "Could Not Write To New File");
 		printf("%d \n", GetLastError());
 		return 0;
 	}
-	CloseHandle(hNewFile);
+	CloseHandle(m_hNewFile);
 	return 1;
 }
 
 
-DWORD FileManagement::GetSize()
+DWORD FileManagement::GetSize() const
 {
-	return dFileSize;
+	return m_dwFileSize;
 }
 
-HANDLE FileManagement::GetHandle()
+HANDLE FileManagement::GetHandle() const
 {
-	return hFile;
+	return m_hFile;
 }
 
-void* FileManagement::GetBytePointer()
+void* FileManagement::GetBytePointer() const
 {
-	return pBytes;
+	return m_pBytes;
 }
